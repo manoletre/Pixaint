6be20724b0c243f0a46c896323db5ea22c0c4f32
Manuel Cárdenas <manoletre@gmail.com>
2017-04-09 16:11:34 -0500

    Fixed linting errors
    Fixed some linting errors the code had (done with eslint)

--- app.js
+++ app.js
@@ -1,109 +1,80 @@
-$(document).on("ready", init());
+var columns;
+var rows;
+var gridW;
+var color;
+var current;
+var painting;
+var selecting;
+var erasing;
+var isDown;
+var start;
+var css;
+var fileNameOriginal;
+var fileName;
+var margins;
+var dividor;
+var wrapperW;
+var wrapperH;
 
-var columns, rows, gridW, color, current, painting, selecting, erasing, isDown, start, css, fileNameOriginal, fileName, margins, dividor, wrapperW, wrapperH;
-
-function init(){
+function init() {
 	start = {};
 	css = {};
 	margins = "ON";
 	dividor = 27;
 	painting = true;
-	color = "#000000"
-	fileNameOriginal = "Best drawing. Ever."
-	$(document).on("mouseup", function(){
-		isDown = false
+	color = "#000000";
+	fileNameOriginal = "Best drawing. Ever.";
+	$(document).on("mouseup", function () {
+		isDown = false;
 	});
-
 }
 
-function newFile(){
-  columns = $("#columns_nr").val();
-	if(columns === ""){
-		alert("how many columns will your file have?");
-		return;
-	}
-  columns = parseInt(columns);
-  rows = $("#rows_nr").val();
-	if(rows === ""){
-		alert("how many rows will your file have?");
-		return;
-	}
-  rows = parseInt(rows);
+$(document).on("ready", init());
 
-	wrapperW = columns * 25;
-	wrapperH = rows * 25;
-
-	fileNameOriginal = $("#file_name").val();
-	if(fileNameOriginal === ""){
-		alert("you need to give your file a name!");
-		return;
+// painting function
+function paint() {
+	if (painting) {
+		current.css("background-color", color);
+		current.html("");
 	}
-	$("title").html(fileNameOriginal);
-
-  $(".alert#new_alert").css("display", "none");
-	actualAlert = undefined;
-  drawGrid()
 }
 
-function drawGrid(){
-  gridW = 0
-  //making sure grid is empy
-	$("#grid").empty();
-
-	//adding rows to grid
-	for (var i = 1; i < rows+1; i++) {
-		$("#grid").append("<div class='row'>");
-	}
-
-	//adding columns to grid (adding squares to rows)
-	for (var i = 1; i < columns+1; i++) {
-		$(".row").append("<div class='grid_square'>");
-    gridW += 27
-	}
-
-	//setting div of grid so that margin:auto works
-  $("#grid").width(gridW)
-
-	//adding x to squares
-	$(".grid_square").html("x")
-
-	gridEvents();
+// function that actually erases
+function erase() {
+	current.css("background-color", "#fff");
+	current.html("x");
 }
 
-function selectColor(){
-	//setting color to selected by user
-	color = $("#select_color").val()
-	$(".alert#color_alert").css("display", "none");
-	actualAlert = undefined;
-	painting = true;
-	erasing = false;
-	selecting = false;
-}
-
-//grid events
-function gridEvents(){
-	$(".grid_square").on("mousedown", function(){
+// grid events
+function gridEvents() {
+	$(".grid_square").on("mousedown", function () {
 		current = $(this);
-		if(painting){paint(), isDown = true}
-		if(erasing){erase(), isDown = true}
-	});
-
-	$(".grid_square").on("mouseover", function(){
-		current = $(this);
-		current.css("opacity", 0.92);
-		if(isDown){
-			if(painting){paint();}
-			if(erasing){erase();}
+		if (painting) {
+			paint();
+			isDown = true;
+		}
+		if (erasing) {
+			erase();
+			isDown = true;
 		}
 	});
 
-	$(".grid_square").on("mouseout", function(){
+	$(".grid_square").on("mouseover", function () {
+		current = $(this);
+		current.css("opacity", 0.92);
+		if (isDown) {
+			if (painting) { paint(); }
+			if (erasing) { erase(); }
+		}
+	});
+
+	$(".grid_square").on("mouseout", function () {
 		current.css("opacity", 1);
 		current = undefined;
 	});
 
-	$(".grid_square").on("click", function(){
-		if(selecting){
+	$(".grid_square").on("click", function () {
+		if (selecting) {
 			start.pixel = this;
 			$("#submit_start_pixel").html("CHANGE START PIXEL");
 			$("#export_alert").css("display", "block");
@@ -111,96 +82,135 @@ function gridEvents(){
 	});
 }
 
-//painting function
-function paint(){
-	if(painting){
-		current.css("background-color", color);
-		current.html("");
+function drawGrid() {
+	gridW = 0;
+  // making sure grid is empy
+	$("#grid").empty();
+
+	// adding rows to grid
+	for (i = 1; i < rows + 1; i += 1) {
+		$("#grid").append("<div class='row'>");
 	}
+
+	// adding columns to grid (adding squares to rows)
+	for (i = 1; i < columns + 1; i += 1) {
+		$(".row").append("<div class='grid_square'>");
+		gridW += 27;
+	}
+
+	// setting div of grid so that margin:auto works
+	$("#grid").width(gridW);
+
+	// adding x to squares
+	$(".grid_square").html("x");
+
+	gridEvents();
 }
 
-//function to start the eraser
-function startEraser(){
+function newFile() {
+	columns = $("#columns_nr").val();
+	if (columns === "") {
+		alert("how many columns will your file have?");
+		return;
+	}
+	columns = parseInt(columns, 10);
+	rows = $("#rows_nr").val();
+	if (rows === "") {
+		alert("how many rows will your file have?");
+		return;
+	}
+	rows = parseInt(rows, 10);
+
+	wrapperW = columns * 25;
+	wrapperH = rows * 25;
+
+	fileNameOriginal = $("#file_name").val();
+	if (fileNameOriginal === "") {
+		alert("you need to give your file a name!");
+		return;
+	}
+	$("title").html(fileNameOriginal);
+
+	$(".alert#new_alert").css("display", "none");
+	actualAlert = undefined;
+	drawGrid();
+}
+
+function selectColor() {
+	// setting color to selected by user
+	color = $("#select_color").val();
+	$(".alert#color_alert").css("display", "none");
+	actualAlert = undefined;
+	painting = true;
+	erasing = false;
+	selecting = false;
+}
+
+// function to start the eraser
+function startEraser() {
 	painting = false;
 	erasing = true;
 	selecting = false;
 }
 
-//function that actually erases
-function erase(){
-	current.css("background-color", "#fff");
-	current.html("x");
-}
-
-//selecting the "start pixel", the pixel where the shadow will come from
-function selectStartPixel(){
+// selecting the "start pixel", the pixel where the shadow will come from
+function selectStartPixel() {
 	selecting = true;
 	erasing = false;
 	painting = false;
 	$("#export_alert").css("display", "none");
 }
 
-//to download file as html
-function downloadHtml(){
-	fileName = fileNameOriginal + ".html";
-	downloadFile();
-}
-
-//to download file as txt
-function downloadTxt(){
-	fileName = fileNameOriginal + ".txt";
-	downloadFile();
-}
-
-function downloadFile(){
+function downloadFile() {
+	var download;
 	start.pixel = $("#grid div:nth-child(1) div:nth-child(1)");
 
-	//calculate distances between squares, works like a family where the center of the
-	//family tree is the start pixel
+	// calculate distances between squares, works like a family where the center of the
+	// family tree is the start pixel
 	start.sib = $(start.pixel).siblings();
 	start.papa = $(start.pixel).parent();
 	start.papa.sib = $(start.papa).siblings();
-	for (var n = 0; n < start.papa.sib.length; n++) {
+	for (n = 0; n < start.papa.sib.length; n += 1) {
 		start.papa.sib[n].sons = $(start.papa.sib[n]).children();
-		for (var i = 0; i < start.papa.sib[n].sons.length; i++) {
+		for (i = 0; i < start.papa.sib[n].sons.length; i += 1) {
 			start.papa.sib[n].sons[i].distance = {
-				side: Math.round(($(start.pixel).offset().left - $(start.papa.sib[n].sons[i]).offset().left)/dividor),
-				top: Math.round(($(start.pixel).offset().top - $(start.papa.sib[n].sons[i]).offset().top)/dividor)
-			}
+				side: Math.round(($(start.pixel).offset().left - $(start.papa.sib[n].sons[i]).offset().left) / dividor),
+				top: Math.round(($(start.pixel).offset().top - $(start.papa.sib[n].sons[i]).offset().top) / dividor)
+			};
 		}
 	}
 
-	for (var i = 0; i < start.sib.length; i++) {
+	for (i = 0; i < start.sib.length; i += 1) {
 		start.sib[i].distance = {
-			side: Math.round(($(start.pixel).offset().left - $(start.sib[i]).offset().left)/dividor),
+			side: Math.round(($(start.pixel).offset().left - $(start.sib[i]).offset().left) / dividor),
 			top: 0
 		};
 	}
 
-	//creating css code
-	css.code = []
-	for (var i = 0; i < start.sib.length; i++) {
-		if(!($(start.sib[i]).html() == "x")){
-			start.sib[i].code = ((-1 * start.sib[i].distance.side * 25)+"px") + " " + ((-1 * start.sib[i].distance.top * 25)+"px") + " 0 " + $(start.sib[i]).css("background-color") + " ";
+	// creating css code
+	css.code = [];
+	for (i = 0; i < start.sib.length; i += 1) {
+		if (!($(start.sib[i]).html() === "x")) {
+			start.sib[i].code = ((-1 * start.sib[i].distance.side * 25) + "px") + " " + ((-1 * start.sib[i].distance.top * 25) + "px") + " 0 " + $(start.sib[i]).css("background-color") + " ";
 			css.code.push(start.sib[i].code);
 		}
 	}
-	for (var n = 0; n < start.papa.sib.length; n++) {
-		for (var i = 0; i < start.papa.sib[n].sons.length; i++) {
-			if(!($(start.papa.sib[n].sons[i]).html() == "x")){
-				start.papa.sib[n].sons[i].code = ((-1 * start.papa.sib[n].sons[i].distance.side * 25)+"px") + " " + ((-1 * start.papa.sib[n].sons[i].distance.top * 25)+"px") + " 0 " + $(start.papa.sib[n].sons[i]).css("background-color") + " ";
+	for (n = 0; n < start.papa.sib.length; n += 1) {
+		for (i = 0; i < start.papa.sib[n].sons.length; i += 1) {
+			if (!($(start.papa.sib[n].sons[i]).html() === "x")) {
+				start.papa.sib[n].sons[i].code = ((-1 * start.papa.sib[n].sons[i].distance.side * 25) + "px") + " " + ((-1 * start.papa.sib[n].sons[i].distance.top * 25) + "px") + " 0 " + $(start.papa.sib[n].sons[i]).css("background-color") + " ";
 				css.code.push(start.papa.sib[n].sons[i].code);
 			}
 		}
 	}
 
-	//creating file to download and downloading it
-	if($(start.pixel).html() == "x"){
+	// creating file to download and downloading it
+	if ($(start.pixel).html() === "x") {
 		html = "<div class='wrapper' id=\"wrapper\"><div class='pixel' id=\"startPixel\"></div></div><style>.wrapper#wrapper{width: " + wrapperW + "px; height: " + wrapperH + "px; } .pixel#startPixel{width: 25px; height: 25px; box-shadow: " + css.code + ";} </style>";
-	}else{
+	} else {
 		html = "<div class='wrapper' id=\"wrapper\"><div class='pixel' id=\"startPixel\"></div></div><style>.wrapper#wrapper{width: " + wrapperW + "px; height: " + wrapperH + "px; } .pixel#startPixel{background-color: " + $(start.pixel).css("background-color") + "; width: 25px; height: 25px; box-shadow: " + css.code + ";} </style>";
 	}
-	var download = new File([html], "download.txt", {type: "text/plain;charset=utf-8"})
+	download = new File([html], "download.txt", { type: "text/plain;charset=utf-8" });
 	saveAs(download, fileName);
 
 	$("#export_alert").css("display", "none");
@@ -208,15 +218,27 @@ function downloadFile(){
 	selecting = false;
 }
 
-function toggleMargins(){
-	if(margins == "ON"){
-		margins = "OFF"
+// to download file as html
+function downloadHtml() {
+	fileName = fileNameOriginal + ".html";
+	downloadFile();
+}
+
+// to download file as txt
+function downloadTxt() {
+	fileName = fileNameOriginal + ".txt";
+	downloadFile();
+}
+
+function toggleMargins() {
+	if (margins === "ON") {
+		margins = "OFF";
 		$("#margins span").html("MARGINS " + margins);
 		$(".row").css("height", "25px");
 		$(".grid_square").css("border", "none");
 		dividor = 25;
-	}else{
-		margins = "ON"
+	} else {
+		margins = "ON";
 		$(".row").css("height", "27px");
 		$(".grid_square").css("border", "1px solid #222");
 		$("#margins span").html("MARGINS " + margins);
--- design.js
+++ design.js
@@ -1,75 +1,67 @@
-$(document).on("ready", init());
-
 var actualAlert;
 
-function init(){
-  $(document).keypress(function(e){
-    if(e.keyCode == '13'){
-        if(actualAlert == "new"){
-          newFile();
-        }else if(actualAlert == "color"){
-          selectColor();
-        }else if(actualAlert == "export"){
-          downloadFile();
-        }
-    }
-});
+function init() {
+	$(document).keypress(function (e) {
+		if (e.keyCode === "13") {
+			if (actualAlert === "new") {
+				newFile();
+			} else if (actualAlert === "color") {
+				selectColor();
+			} else if (actualAlert === "export") {
+				downloadFile();
+			}
+		}
+	});
 }
 
-//to display the 'new file' alert and stop displaying the other alerts
-function newAlert(){
-  actualAlert = "new";
-  $(".alert#new_alert").css("display", "block");
-  $(".alert#color_alert").css("display", "none");
-  $(".alert#export_alert").css("display", "none");
+$(document).on("ready", init());
+
+// to display the "new file" alert and stop displaying the other alerts
+function newAlert() {
+	actualAlert = "new";
+	$(".alert#new_alert").css("display", "block");
+	$(".alert#color_alert").css("display", "none");
+	$(".alert#export_alert").css("display", "none");
 }
 
-//to display the 'select color' alert and stop displaying the other alerts
-function colorAlert(){
-  actualAlert = "color";
-  $(".alert#new_alert").css("display", "none");
-  $(".alert#color_alert").css("display", "block");
-  $(".alert#export_alert").css("display", "none");
+// to display the "select color" alert and stop displaying the other alerts
+function colorAlert() {
+	actualAlert = "color";
+	$(".alert#new_alert").css("display", "none");
+	$(".alert#color_alert").css("display", "block");
+	$(".alert#export_alert").css("display", "none");
 
-  $("#select_color").on("change", function(){
-    var colorValue = $("#select_color").val();
+	$("#select_color").on("change", function () {
+		var colorValue = $("#select_color").val();
 		$("#text_color").val(colorValue);
-	})
-  $("#text_color").on("input", function(){
-    var colorValue = $("#text_color").val();
+	});
+	$("#text_color").on("input", function () {
+		var colorValue = $("#text_color").val();
 		$("#select_color").val(colorValue);
-	})
+	});
 }
 
-//to display the 'export to css' alert and stop displaying the other alerts
-function exportAlert(){
-  actualAlert = "export";
-  $(".alert#new_alert").css("display", "none");
-  $(".alert#color_alert").css("display", "none");
-  $(".alert#export_alert").css("display", "block");
+// to display the "export to css" alert and stop displaying the other alerts
+function exportAlert() {
+	actualAlert = "export";
+	$(".alert#new_alert").css("display", "none");
+	$(".alert#color_alert").css("display", "none");
+	$(".alert#export_alert").css("display", "block");
 }
 
-//to exit the alert (it will stop displaying it)
-function exitAlert(){
-  if(actualAlert == "new"){
-    $(".alert#new_alert").css("display", "none");
-  }else if(actualAlert == "color"){
-    $(".alert#color_alert").css("display", "none");
-  }else if(actualAlert == "export"){
-    $(".alert#export_alert").css("display", "none");
-  }
-  actualAlert = undefined;
+// to exit the alert (it will stop displaying it)
+function exitAlert() {
+	if (actualAlert === "new") {
+		$(".alert#new_alert").css("display", "none");
+	} else if (actualAlert === "color") {
+		$(".alert#color_alert").css("display", "none");
+	} else if (actualAlert === "export") {
+		$(".alert#export_alert").css("display", "none");
+	}
+	actualAlert = undefined;
 }
 
-//to rotate or stop rotating the options logo
-function rotate(){
-  $("#credits").css("display", "block");
-}
-function stopRotating(){
-  $("#credits").css("display", "none");
-}
-
-//to launch the help site
-function help(){
-  window.open('help.html','_blank');
+// to launch the help site
+function help() {
+	window.open("help.html", "_blank");
 }
